8215ec85817b122ae85fa76e2a086179616e3e7e
elpguillen <elpguillen@users.noreply.github.com>
2025-08-09 22:08:04 -0700

    Comment solution to LeetCode problem "Binary Tree Postorder Traversal"

--- LeetCode/BinaryTreePostorderTraversal/SolutionJava.java
+++ LeetCode/BinaryTreePostorderTraversal/SolutionJava.java
@@ -15,8 +15,16 @@
  */
 class Solution {
     
+    // A list to store the nodes' values in postorder sequence (left -> right -> root)
     private List<Integer> result = new ArrayList<Integer>();
     
+    /**
+     * Performs a postorder traversal on a binary tree starting from the given root.
+     * Postorder traversal order: traverse left subtree, traverse right subtree, visit node.
+     * 
+     * @param root The root node of the binary tree.
+     * @return A list containing the values of nodes in postorder traversal order.
+     */
     public List<Integer> postorderTraversal(TreeNode root) {
         
         if (root == null)
