4cd9e0d3f9673337131d258f5d803336b2bb92da
elpguillen <elpguillen@users.noreply.github.com>
2025-08-13 17:59:19 -0700

    Add solution to LeetCode problem "Add Two Numbers" written in Java

---
+++ LeetCode/AddTwoNumbers/SolutionJava.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        int remainder = 0;
+        int carry = 0;
+
+        ListNode head = new ListNode();
+        ListNode curr = head;
+
+        while ( l1 != null || l2 != null || carry > 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+
+            remainder = sum % 10;
+            carry = sum / 10;
+
+            curr.next = new ListNode(remainder);
+            curr = curr.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        
+        return head.next;
+    }
+}
