c2256898e5a9f4be90361fcbb49c56e6589a7331
elpguillen <elpguillen@users.noreply.github.com>
2025-09-18 19:34:48 -0700

    Add comments to explain main steps in algorithm.

--- LeetCode/MergeSortedArray/SolutionJava.java
+++ LeetCode/MergeSortedArray/SolutionJava.java
@@ -8,6 +8,7 @@ class Solution {
         int pointerIndex2 = n - 1;
         int nextItem = m + n - 1;
         
+        // Compare elements from the end of both arrays and insert the larger one at the end of nums1.
         while (pointerIndex1 >= 0 && pointerIndex2 >= 0) {
             if(nums1[pointerIndex1] > nums2[pointerIndex2])
                 nums1[nextItem--] = nums1[pointerIndex1--];
@@ -15,10 +16,12 @@ class Solution {
                 nums1[nextItem--] = nums2[pointerIndex2--];
         }
         
+        // If there are remaining elements in nums1, copy them over (though they are already in place).
         while (pointerIndex1 >= 0) {
             nums1[nextItem--] = nums1[pointerIndex1--];
         }
         
+        // If there are remaining elements in nums2, copy them into nums1.
         while (pointerIndex2 >= 0) {
             nums1[nextItem--] = nums2[pointerIndex2--];
         }
