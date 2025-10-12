7f420952dc6f5e98dd4d21a168937df0785fc74a
elpguillen <elpguillen@users.noreply.github.com>
2025-10-11 22:49:54 -0700

    Add solution to LeetCode problem "Pow(x,n)" in Java

---
+++ LeetCode/Pow/SolutionJava.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // Handle negative powers by inverting x and flipping n
+        long N = n; // Use long to handle Integer.MIN_VALUE case
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        return fastPow(x, N);
+    }
+
+    private double fastPow(double x, long n) {
+        if (n == 0)
+            return 1.0;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+}
